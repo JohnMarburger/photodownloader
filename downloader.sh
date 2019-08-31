bc3c974444c77bb455ab9305ee082cd97be5b9dc
JohnMarburger <54755706+JohnMarburger@users.noreply.github.com>
2019-08-31 19:45:36 -0400

    Create downloader.sh

---
+++ downloader.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+mkdir nasaphotos
+cd nasaphotos
+wget https://solarsystem.nasa.gov/download_all/all_raw_images_0.zip
+wget https://solarsystem.nasa.gov/download_all/all_raw_images_1.zip
+wget https://solarsystem.nasa.gov/download_all/all_raw_images_2.zip
+wget https://solarsystem.nasa.gov/download_all/all_raw_images_3.zip
+wget https://solarsystem.nasa.gov/download_all/all_raw_images_4.zip
+wget https://solarsystem.nasa.gov/download_all/all_raw_images_5.zip
+wget https://solarsystem.nasa.gov/download_all/all_raw_images_6.zip
+wget https://solarsystem.nasa.gov/download_all/all_raw_images_7.zip
+echo "all done"
